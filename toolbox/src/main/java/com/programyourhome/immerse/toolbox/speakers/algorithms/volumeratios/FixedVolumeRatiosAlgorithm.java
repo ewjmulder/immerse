648fc286e185a6bdd5a5d3583ed32e716d8713db
Erik Mulder <ewjmulder@yahoo.com>
2018-05-09 10:49:43 +0200

    Play a scenario at an individual speaker directly, closes #29

--- toolbox/src/main/java/com/programyourhome/immerse/toolbox/speakers/algorithms/volumeratios/FixedVolumeRatiosAlgorithm.java
+++ toolbox/src/main/java/com/programyourhome/immerse/toolbox/speakers/algorithms/volumeratios/FixedVolumeRatiosAlgorithm.java
@@ -1,8 +1,12 @@
 package com.programyourhome.immerse.toolbox.speakers.algorithms.volumeratios;
 
+import java.util.stream.Collectors;
+
 import com.programyourhome.immerse.domain.Factory;
+import com.programyourhome.immerse.domain.Room;
 import com.programyourhome.immerse.domain.Serialization;
 import com.programyourhome.immerse.domain.Snapshot;
+import com.programyourhome.immerse.domain.speakers.Speaker;
 import com.programyourhome.immerse.domain.speakers.SpeakerVolumeRatios;
 import com.programyourhome.immerse.domain.speakers.algorithms.volumeratios.VolumeRatiosAlgorithm;
 
@@ -26,6 +30,15 @@ public class FixedVolumeRatiosAlgorithm implements VolumeRatiosAlgorithm {
 
     public static Factory<VolumeRatiosAlgorithm> fixed(SpeakerVolumeRatios speakerVolumeRatios) {
         return () -> new FixedVolumeRatiosAlgorithm(speakerVolumeRatios);
+    }
+
+    /**
+     * Map all speakers to volume 0, just the speaker with the given id to volume 1.
+     */
+    public static Factory<VolumeRatiosAlgorithm> justSpeaker(Room room, int speakerId) {
+        SpeakerVolumeRatios justSpeakerVolumeRatios = new SpeakerVolumeRatios(
+                room.getSpeakers().values().stream().collect(Collectors.toMap(Speaker::getId, speaker -> speaker.getId() == speakerId ? 1.0 : 0.0)));
+        return () -> new FixedVolumeRatiosAlgorithm(justSpeakerVolumeRatios);
     }
 
 }
