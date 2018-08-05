b4a6c9e95d443f9c30f0273db0d327c983c7a995
Erik Mulder <ewjmulder@yahoo.com>
2018-08-05 22:43:39 +0200

    Add Start class with main method to start network server

---
+++ network-server/src/main/java/com/programyourhome/immerse/network/server/Start.java
@@ -0,0 +1,31 @@
+package com.programyourhome.immerse.network.server;
+
+import org.pmw.tinylog.Configurator;
+import org.pmw.tinylog.Level;
+import org.pmw.tinylog.writers.ConsoleWriter;
+
+/**
+ * The Start class contains a main method to start the Immerse Network Server.
+ */
+public class Start {
+
+    public static final int DEFAULT_PORT = 19161;
+
+    public static void main(String[] args) {
+        // TODO: print usage if --help
+        // TODO: make dynamic
+        Configurator.defaultConfig()
+                .formatPattern("{date:yyyy-MM-dd HH:mm:ss} [{thread}] {class_name}.{method}() - {level}: {message}")
+                .writer(new ConsoleWriter())
+                .level(Level.DEBUG)
+                .activate();
+
+        int port = DEFAULT_PORT;
+        if (args.length > 0) {
+            // TODO: handle error
+            port = Integer.parseInt(args[0]);
+        }
+        new ImmerseServer(port).start();
+    }
+
+}
