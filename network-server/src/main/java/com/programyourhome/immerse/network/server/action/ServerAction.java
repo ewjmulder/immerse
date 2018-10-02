1c643f14b3f2dc72ca72cb1068dac9ed26030da7
Erik Mulder <ewjmulder@yahoo.com>
2018-10-02 23:16:49 +0200

    Allow for async handling of all actions but server starting/stopping

--- network-server/src/main/java/com/programyourhome/immerse/network/server/action/ServerAction.java
+++ network-server/src/main/java/com/programyourhome/immerse/network/server/action/ServerAction.java
@@ -6,32 +6,38 @@ package com.programyourhome.immerse.network.server.action;
 public enum ServerAction {
 
     /** Create a new mixer. */
-    CHECK_CONNECTION(new CheckConnectionAction()),
+    CHECK_CONNECTION(new CheckConnectionAction(), true),
     /** Create a new mixer. */
-    HAS_MIXER(new HasMixerAction()),
+    HAS_MIXER(new HasMixerAction(), true),
     /** Create a new mixer. */
-    CREATE_MIXER(new CreateMixerAction()),
+    CREATE_MIXER(new CreateMixerAction(), false),
     /** Start the mixer. */
-    START_MIXER(new StartMixerAction()),
+    START_MIXER(new StartMixerAction(), false),
     /** Get the settings. */
-    GET_SETTINGS(new GetSettingsAction()),
+    GET_SETTINGS(new GetSettingsAction(), true),
     /** Play a scenario. */
-    PLAY_SCENARIO(new PlayScenarioAction()),
+    PLAY_SCENARIO(new PlayScenarioAction(), true),
     /** Wait for a playback to finish. */
-    WAIT_FOR_PLAYBACK(new WaitForPlaybackAction()),
+    WAIT_FOR_PLAYBACK(new WaitForPlaybackAction(), true),
     /** Stop playback. */
-    STOP_PLAYBACK(new StopPlaybackAction()),
+    STOP_PLAYBACK(new StopPlaybackAction(), true),
     /** Stop the mixer. */
-    STOP_MIXER(new StopMixerAction());
+    STOP_MIXER(new StopMixerAction(), false);
 
     private Action<?> action;
+    private boolean canRunAsync;
 
-    private ServerAction(Action<?> action) {
+    private ServerAction(Action<?> action, boolean canRunAsync) {
         this.action = action;
+        this.canRunAsync = canRunAsync;
     }
 
     public Action<?> getAction() {
         return this.action;
+    }
+
+    public boolean isCanRunAsync() {
+        return this.canRunAsync;
     }
 
 }
